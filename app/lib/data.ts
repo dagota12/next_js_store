1a4882e81d72666b7c34874cb46ad994f82f540e
dagota12 <chernetdagim@gmail.com>
2024-03-22 13:23:35 +0300

    bug with unstable_noStore removed some calls of the function

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -175,7 +175,6 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
-  noStore();
   try {
     const data = await sql<CustomerField>`
       SELECT
@@ -194,7 +193,6 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
-  noStore();
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
@@ -228,7 +226,6 @@ export async function fetchFilteredCustomers(query: string) {
 }
 
 export async function getUser(email: string) {
-  noStore();
   try {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0] as User;
